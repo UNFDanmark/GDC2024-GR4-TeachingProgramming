c08ec6f07fbbd3dcbeafe133be0f8187bc9a1376
Deltager <teknisk+GDC2024-GR4@game.unf.dk>
2024-07-12 14:52:12 +0200

    Evil and good player

---
+++ Opgaver/Assets/Evil_behaviour.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evil_behaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hej, jeg er ond!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
